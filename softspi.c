614b4ff2c0c51d2ee0ad2834d60e00b0285fedf5
hanishkvc <hanishkvc@gmail.com>
2012-05-07 12:15:10 +0530

    v0.01 * Added the basic soft spi logic * It uses 4 GPIO on a uC or SOC to act as the SPI lines * It assumes there is few gio handling functions like   * setgio,   * getgio and   * setpinmux   as well as a   * Delay function   already defined for the target. Or else the same requires   to be implemented. * All the 4 SPI modes have been implemented * Currently it supports a Full duplex 32 bit (Max) transfer   One requires to set the Out variable with the value required   Tell what is the maximum number of bits in this full duplex   transfer (limited to a absolute max of 32 bits).   * Note if out is only required for a few clocks out of the     total clocks executed to include logical input and output     in which ever order with in the over all physical transfer,     THEN ensure that out has some valid NOP values during the     remaining period of the transfer.

---
+++ softspi.c
@@ -0,0 +1,148 @@
+/* Soft SPI - a library for software gpio based SPI interaction
+ * v07May2012_1033
+ * HanishKVC
+ */
+
+#define U32 unsigned long
+
+/*
+ * Define these if you want to call sspi_init to setup
+ * the PINMUX for Soft SPI GPIOs
+ */
+
+#define PMUX_DIR_IN
+#define PMUX_DIR_OUT
+#define PMUX_DIR_IO
+#define PMUX_PULL_UP
+#define PMUX_PULL_DOWN
+
+/*
+ * One needs to define these with the GPIO numbers associated with
+ * Soft SPI h/w lines
+ */
+#define GIO_CLK
+#define GIO_CS
+#define GIO_MISO
+#define GIO_MOSI
+
+#define CS_ACTIVE 0
+#define CS_INACTIVE 1
+
+
+/* 
+ * Define the delay function
+ * And define the below mentioned delays as required for the specified delay function
+ */
+
+#define DELAYFUNC
+#define DELAY_AFTER_CS 100
+#define DELAY_AFTER_BYTE 100
+#define DELAY_BETWEEN_BITS 50
+
+/* Define the SPI mode as required */
+#define SPI_CPOL 0
+#define SPI_CPHA 0
+
+/* 
+ * define the below GIO functions as required
+ * The Soft SPI logic uses these GIO functions
+ */
+int getgio(int gio);
+void setgio(int gio, int val);
+void setpinmux(int gio, int flags);
+
+/*
+ * Setup soft spi gio/gpios if not already done
+ */
+
+void sspi_init()
+{
+  setpinmux(GIO_CLK, PMUX_DIR_OUT | PMUX_PULL_UP);
+  setpinmux(GIO_CS, PMUX_DIR_OUT | PMUX_PULL_UP); // Assuming Slave select is Active low else PMUX_PULL_DOWN
+  setpinmux(GIO_MOSI, PMUX_DIR_OUT | PMUX_PULL_UP);
+  setpinmux(GIO_MISO, PMUX_DIR_IN | PMUX_PULL_UP); // Should result in input being 1 when Slave/Spi device is not driving their output
+}
+
+/* Full duplex upto 32bit IO, MSb first
+ * This does a 32 bit trasfer in both direction parallely/simultaneously
+ * So setup the out as required i.e
+ * Dummy OutVals, Actual OutVals
+ * Dummy OutVals, Actual OutVals, Dummy OutVals
+ * Actual OutVals, DummyOutVals
+ * Or any other combinations within 32 bits
+ */
+void sspi_fd_io32(U32 out, U32 *in, int maxbits)
+{
+  U32 tIn;
+  int tInBit;
+
+  setgio(GIO_CS, CS_ACTIVE); // Select the slave
+  DELAYFUNC(DELAY_AFTER_CS);
+  tIn = 0;
+  for(i = 0; i < maxbits; i++) {
+    if(SPI_CPHA == 0) {
+
+      if(out & 0x80000000)
+        setgio(GIO_MOSI, 1);
+      else
+        setgio(GIO_MOSI, 0);
+      DELAYFUNC(DELAY_BETWEEN_BITS*2);
+
+      if(SPI_CPOL == 0)
+        setgio(GIO_CLK, 1);
+      else
+        setgio(GIO_CLK, 0);
+      DELAYFUNC(DELAY_BETWEEN_BITS);
+
+      tIn <<= 1;
+      tInBit = getgio(GIO_MISO);
+      if(tInBit) {
+        tIn |= 1;
+      } else {
+        tIn &= 0xFFFFFFFE;
+      }
+      DELAYFUNC(DELAY_BETWEEN_BITS);
+
+      if(SPI_CPOL == 0)
+        setgio(GIO_CLK, 0);
+      else
+        setgio(GIO_CLK, 1);
+      //DELAYFUNC(DELAY_BETWEEN_BITS);
+
+
+    } else {
+
+      if(SPI_CPOL == 0)
+        setgio(GIO_CLK, 1);
+      else
+        setgio(GIO_CLK, 0);
+
+      if(out & 0x80000000)
+        setgio(GIO_MOSI, 1);
+      else
+        setgio(GIO_MOSI, 0);
+      DELAYFUNC(DELAY_BETWEEN_BITS*2);
+
+
+      if(SPI_CPOL == 0)
+        setgio(GIO_CLK, 0);
+      else
+        setgio(GIO_CLK, 1);
+      tIn <<= 1;
+      tInBit = getgio(GIO_MISO);
+      if(tInBit) {
+        tIn |= 1;
+      } else {
+        tIn &= 0xFFFFFFFE;
+      }
+      DELAYFUNC(DELAY_BETWEEN_BITS*2);
+
+    }
+
+    out <<= 1;
+    if((i%8 == 0) && (i != 0))
+      DELAYFUNC(DELAY_AFTER_BYTE);
+  }
+  *in = tIn;
+}
+
